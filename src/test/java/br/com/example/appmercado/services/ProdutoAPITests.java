1ef34818e832c39ab5e3d03322a332a6a666dfaf
anajorge <anajessicadegj@gmail.com>
2022-11-01 14:45:18 -0300

    update

--- src/test/java/br/com/example/appmercado/services/ProdutoAPITests.java
+++ src/test/java/br/com/example/appmercado/services/ProdutoAPITests.java
@@ -13,7 +13,6 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
 public class ProdutoAPITests {
     @Autowired
     private MockMvc mvc;
